71522b1f4bbe147de454d10d873c4f9254e98f60
JASON PINELO <st1809139@upy.edu.mx>
2021-06-24 00:58:57 -0500

    Finishing configuration to execute all updates on the login page

--- setsuperuser.sh
+++ setsuperuser.sh
@@ -4,3 +4,5 @@
 python manage.py migrate
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('master', 'alekarhurjas@gmail.com', 'theoneaboveall')" | python manage.py shell
+
+python manage.py runserver 0.0.0.0:8000
