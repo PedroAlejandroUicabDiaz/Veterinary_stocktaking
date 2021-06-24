be00620e313a78699305bcdf40a75d9532dd96c6
JASON PINELO <st1809139@upy.edu.mx>
2021-06-24 01:21:17 -0500

    Adding wait-for-it.sh

--- setsuperuser.sh
+++ setsuperuser.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Automatin migration and set of superuser
 
+python manage.py runserver 0.0.0.0:8000
+
 python manage.py migrate
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('master', 'alekarhurjas@gmail.com', 'theoneaboveall')" | python manage.py shell
-
-python manage.py runserver 0.0.0.0:8000
