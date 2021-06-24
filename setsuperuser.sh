fb27d1456c2659dcdaece1e71d70df3d449610c8
JASON PINELO <st1809139@upy.edu.mx>
2021-06-24 12:13:53 -0500

    Adding ensure_adminuser.py

--- setsuperuser.sh
+++ setsuperuser.sh
@@ -3,8 +3,6 @@
 
 python manage.py migrate
 
-echo "from django.contrib.auth.models import User; \
-    User.objects.create_superuser('master', 'alekarhurjas@gmail.com', 'theoneaboveall')" \
-    | python manage.py shell
+python manage.py ensure-adminuser.py --user=master --password=theoneaboveall
 
 python manage.py runserver 0.0.0.0:8000
