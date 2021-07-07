a69a665833b3a624fd95890f1943373079f0350e
PedroAlejandroUicabDiaz <1909180@upy.edu.mx>
2021-07-07 01:43:38 -0500

    rename .sh_file and new filters file was added, view modification

---
+++ dashapp/filters.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+import django_filters
+from .models import *
+
+class StocktakingFilter(django_filters.FilterSet):
+    class Meta:
+        model = stocktaking_tb
+        fields = '__all__'
--- dashapp/views.py
+++ dashapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import *
+from .filters import StocktakingFilter
+# from .forms import 
 
 # Create your views here.
