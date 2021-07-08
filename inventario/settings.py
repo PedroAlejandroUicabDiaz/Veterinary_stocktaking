699a8ff4f17ccb2f4371587f81f747f0c28f51a3
PedroAlejandroUicabDiaz <1909180@upy.edu.mx>
2021-07-07 23:47:50 -0500

    returning to the good state

--- inventario/settings.py
+++ inventario/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 LOGIN_REDIRECT_URL = '/dashapp/dash/' # Una vez que haces login vas al dashboard
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
