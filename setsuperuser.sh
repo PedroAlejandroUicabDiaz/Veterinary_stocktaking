018a19ea9f957bd0cba54a6c32b45c075e49153d
Jason Pinelo <st1809139@upy.edu.mx>
2021-07-17 21:09:15 -0500

    First try for nginx

--- setsuperuser.sh
+++ setsuperuser.sh
@@ -7,4 +7,6 @@ python manage.py migrate
 
 python manage.py ensure_adminuser --user=master --password=theoneaboveall
 
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+
+gunicorn sampleApp.wsgi:application --bind 0.0.0.0:8000
