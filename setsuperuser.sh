31da400d517846654db07be4a04eadc4bd57f963
Jason Pinelo <st1809139@upy.edu.mx>
2021-07-17 21:45:54 -0500

    nginx now working :)

--- setsuperuser.sh
+++ setsuperuser.sh
@@ -9,4 +9,4 @@ python manage.py ensure_adminuser --user=master --password=theoneaboveall
 
 # python manage.py runserver 0.0.0.0:8000
 
-gunicorn sampleApp.wsgi:application --bind 0.0.0.0:8000
+gunicorn inventario.wsgi:application --bind 0.0.0.0:8000
